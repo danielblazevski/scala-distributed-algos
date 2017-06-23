9c6495af7118b603a32fe48e3d70f168746ad940
Daniel Blazevski <daniel.blazevski@gmail.com>
2017-06-23 13:35:21 -0400

    replaced logic with HS and cleaned up data structures

--- election-leader-ring/src/main/scala/election/electionLeader.scala
+++ election-leader-ring/src/main/scala/election/electionLeader.scala
@@ -11,9 +11,16 @@ import scala.collection.mutable.HashMap
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-case class node(id: Int, leaderGuess: Int, leftNeighbor: Int, rightNeighbor: Int)
-case class askLeader(id: Int)
-case class updateLeader(id: Int)
+class node(var id: Int, var leaderGuess: Int, var leftNeighbor: node, var rightNeighbor: node){
+  // need to override hashcode to avoid stackoverflow in HashSet
+  override def hashCode : Int = {
+    this.id
+  }
+}
+
+case class isLeaderComp(from: node, to: node)
+case class passLeaderGuess(from: node, to: node)
+
 
 object MyImplicits{
   implicit val timeout = Timeout(1 second)
@@ -22,9 +29,6 @@ object MyImplicits{
 class Ring(numNodes: Int, system: ActorSystem, nodes: Array[node]) {
   import MyImplicits._
 
-  var nodesMap = new HashMap[Int, node]
-  nodes.foreach{n => nodesMap(n.id) = n}
-
   val actorMap = new HashMap[Int, ActorRef]()
   nodes.foreach { n =>
     actorMap(n.id) = system.actorOf(Props(new NodeActor), name = n.id.toString)
@@ -32,47 +36,95 @@ class Ring(numNodes: Int, system: ActorSystem, nodes: Array[node]) {
 
   class NodeActor extends Actor {
     def receive = {
-      case askLeader(from: Int) => {
-        sender ! nodesMap(from).leaderGuess
+
+      case passLeaderGuess(from: node, to: node) => {
+        if (from.leaderGuess >= to.leaderGuess) {
+          to.leaderGuess = from.leaderGuess
+        }
+        sender ! (from.leaderGuess >= to.leaderGuess)
       }
     }
   }
 
-  def findLeader(): Int = {
-    var inAgreement = false
-    while (!inAgreement){
-      inAgreement = nodesMap.forall{ nTup =>
-        val n = nTup._2
-        val future = actorMap(n.id) ? askLeader(n.leftNeighbor)
-        val result = Await.result(future, timeout.duration).asInstanceOf[Int]
-        result == n.leaderGuess
-      }
+  // Essentially the Hirschberg and Sinclair algorithm
+  // developed to minimize the number of time we pass tokens to nodes.
+  def findLeaderNlogN(): Int = {
+    // send out info to left and right 2^k times
+    // stop sending if we find something larger
+    // if we make it to the end of the out phase, send message back in
+    // a process does not send messages again if it did not receive both incoming messages
 
-      // else update neighbors on guess
-      nodes.foreach{ n =>
-        val futureLeft  = actorMap(n.id) ? askLeader(n.leftNeighbor)
-        val futureRight  = actorMap(n.id) ? askLeader(n.rightNeighbor)
-        val leaderLeftGuess = Await.result(futureLeft, timeout.duration).asInstanceOf[Int]
-        val leaderRightGuess = Await.result(futureRight, timeout.duration).asInstanceOf[Int]
-        val curLeaderGuess = Array(n.leaderGuess, leaderLeftGuess, leaderRightGuess).min
+    var nodeSet = new scala.collection.mutable.HashSet[node]()
+    nodes.foreach{n => nodeSet.add(n)}
 
-        nodesMap(n.id) = node(n.id, curLeaderGuess, n.leftNeighbor, n.rightNeighbor)
+    var foundLeader = false
+    var phase = 0
+    var leader = 100000000
+    while (!foundLeader) {
+
+      nodeSet.foreach { n =>
+        var currLeft = n
+        var currRight = n
+
+        val allOut = (1 to Math.pow(2, phase).toInt).forall { i =>
+          val futureLeft = actorMap(currLeft.id) ? passLeaderGuess(currLeft, currLeft.leftNeighbor)
+          val resultLeft = Await.result(futureLeft, timeout.duration).asInstanceOf[Boolean]
+          val futureRight = actorMap(currRight.id) ? passLeaderGuess(currRight, currRight.rightNeighbor)
+          val resultRight = Await.result(futureRight, timeout.duration).asInstanceOf[Boolean]
+          val returnRes = resultLeft && resultRight
+          currLeft = currLeft.leftNeighbor
+          currRight = currRight.rightNeighbor
+          if ( (currLeft.id == n.id) && (n.leaderGuess == n.id)) {
+            println("leader found! " +  n.id)
+            leader = n.id
+            foundLeader = true
+          }
+          returnRes
+        }
+        if (!allOut) {
+          nodeSet.remove(n)
+        }
       }
+      phase = phase + 1
     }
-    nodes(0).leaderGuess
+    leader
   }
 }
+
 
 object Main extends App {
   val numNodes = 10
 
-  var nodes = (0 to numNodes).map{x =>
-    node(x,x, Math.floorMod(x + 1, numNodes), Math.floorMod(x - 1, numNodes))
-  }.toArray
+  var root = new node(0, 0, null, null)
+  var current = root
+  var nodeMap = new scala.collection.mutable.HashMap[Int, node]()
+  nodeMap(0) = current
+  (1 until numNodes).foreach{ i =>
+    current.leftNeighbor = new node(i, i, null, null)
+    current = current.leftNeighbor
+    nodeMap(current.id) = current
+ }
+  current.leftNeighbor = root
+
+  // hacky, just using a map to define right neighbors for now
+  current = root
+  (0 until numNodes).foreach{ i =>
+    current.rightNeighbor = nodeMap(numNodes  - 1 - i)
+    current = current.rightNeighbor
+  }
+
+  // put in Array, since don't like the idea of the network taking "root" as a input
+  // since that signals priority to a node
+  val nodes = new Array[node](numNodes)
+  current = root
+  (0 until numNodes).foreach{ i =>
+    nodes(i) = current
+    current = current.leftNeighbor
+  }
 
   val system = ActorSystem("ElectionSystem")
   val network = new Ring(numNodes, system, nodes)
-  val leader = network.findLeader()
+  val leader = network.findLeaderNlogN()
   println(leader)
   println(s"leader was found to be: ${leader}")
   system.shutdown
