19a028c542fb8dc41ee21627d74057e62faaff8c
Daniel Blazevski <daniel.blazevski@gmail.com>
2018-06-21 17:18:48 -0400

    added comments in main

--- election-leader-ring/src/main/scala/election/electionLeaderFinagleServer.scala
+++ election-leader-ring/src/main/scala/election/electionLeaderFinagleServer.scala
@@ -14,10 +14,12 @@ object electionLeaderFinagleServer  {
     val numPorts = args(2).toInt
     println(s"starting server with id = ${id} on port = ${portEnding}")
 
+    // setup server to process requests
     val isLeaderService = new isLeader(id, portEnding.toInt)
     val server = Http.serve(s":80${portEnding}", isLeaderService)
 
     Thread.sleep(10000)
+    // separte thread that checks if is the leader
     checkLeaderClient.checkIfLeader(portEnding.toInt, id, numPorts)
 
     Await.ready(server)
